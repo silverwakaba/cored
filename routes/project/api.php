694c2ad6cb72559c6bb01612b6609f38104e7921
Azhar <azhar.afrp@gmail.com>
2026-01-06 14:57:15 +0700

    routing

--- routes/project/api.php
+++ routes/project/api.php
@@ -1,3 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+// Controller
+// add controller here
+
+// API routing project
+Route::prefix('/')->name('be.')->group(function(){
+    // Project
+    Route::prefix('project')->name('project.')->group(function(){
+        // 
+    });
+});
--- routes/project/web.php
+++ routes/project/web.php
@@ -1,3 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+// Controller
+// add controller here
+
+// FE routing project
+Route::prefix('/')->name('fe.')->middleware(['jwt.global', 'minify.blade'])->group(function(){
+    // Project
+    Route::name('project.')->group(function(){
+        // 
+    });
+});
